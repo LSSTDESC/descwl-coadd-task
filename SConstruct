34524f29d8764b19b69346686f9e17ad2c67c39a
Arun Kannawadi <arunkannawadi@astro.princeton.edu>
2021-08-02 14:58:35 -0500

    Modify associated files

--- SConstruct
+++ SConstruct
@@ -1,4 +1,3 @@
 # -*- python -*-
 from lsst.sconsUtils import scripts
-
 scripts.BasicSConstruct("pipe_task", disableCc=True)
--- bin.src/coaddInCellsTask.py
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env python
-from lsstdesc.pipe.task.coadd_in_cells import CoaddInCellsTask
-CoaddInCellsTask.parseAndRun()
