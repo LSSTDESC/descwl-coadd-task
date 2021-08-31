7469d467bed4232922bcfce1edaf6a0900a24fc9
Arun Kannawadi <arunkannawadi@astro.princeton.edu>
2021-08-31 12:04:17 -0500

    Pass seed value via config override

--- run.sh
+++ run.sh
@@ -5,5 +5,5 @@
 export REPO=/repo/main
 export INPUT_COLLECTION=HSC/runs/RC2/w_2021_30/DM-31182
 setup -k -r .
-pipetask run -t lsstdesc.descwl.coadd.coadd_in_cells.CoaddInCellsTask -b $REPO --input $INPUT_COLLECTION --output u/$USER/coaddTest -d "skymap='hsc_rings_v1' AND tract=9615 AND patch=45 AND band='r'" --register-dataset-types
+pipetask run -t lsstdesc.descwl.coadd.coadd_in_cells.CoaddInCellsTask -b $REPO --input $INPUT_COLLECTION --output u/$USER/coaddTest -d "skymap='hsc_rings_v1' AND tract=9615 AND patch=45 AND band='r'" --config coaddsInCellsV1:seed=12345678 --register-dataset-types
 butler query-datasets $REPO --collections u/$USER/coaddTest/* coaddsInCellsV1
